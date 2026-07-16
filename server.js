c799ee4d48932adfeb2e2b5849ec5a845a9d475a
sreeahillan <sreeahillan2@gmail.com>
2026-07-16 11:46:09 +0530

    i have added post and get requests partially

--- server.js
+++ server.js
@@ -4,6 +4,38 @@ import body from "body-parser";
 const app = express();
 const port = 3001;
 
+app.use(body.urlencoded({extended : true}));
+
+var showtitle = "";
+var showcontent = "";
+
+app.get("/" , (req , res) => {
+    res.render("index.ejs");
+});
+
+app.get("/new" , (req , res) => {
+    res.render("newpost.ejs");
+});
+
+
+
+
+app.post("/new" , (req , res) => {
+    showcontent = req.body.content;
+    showtitle = req.body.title;
+    res.render("index.ejs" , {
+        "title" : showtitle,
+        "content" : showcontent
+    });  
+});
+app.get("/edit" , (req , res) => {
+    res.render("edit.ejs" , {
+        "title" : showtitle
+    });
+});
+
+
+
 app.listen(port , () => {
     console.log(`listening on port ${port} !`);
 })
