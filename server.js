64da7ce6c253097bf448b400eca902197ce48b07
sreeahillan <sreeahillan2@gmail.com>
2026-07-17 14:13:25 +0530

    updated the server.js and the basics action of the project

--- server.js
+++ server.js
@@ -4,38 +4,57 @@ import body from "body-parser";
 const app = express();
 const port = 3001;
 
-app.use(body.urlencoded({extended : true}));
+app.set("viewengine", "ejs");
 
-var showtitle = "";
-var showcontent = "";
+app.use(body.urlencoded({ extended: true }));
 
-app.get("/" , (req , res) => {
-    res.render("index.ejs");
+var array = [];
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
-app.get("/new" , (req , res) => {
-    res.render("newpost.ejs");
+app.get("/new", (req, res) => {
+  res.render("newpost.ejs");
 });
 
-
-
-
-app.post("/new" , (req , res) => {
-    showcontent = req.body.content;
-    showtitle = req.body.title;
-    res.render("index.ejs" , {
-        "title" : showtitle,
-        "content" : showcontent
-    });  
-});
-app.get("/edit" , (req , res) => {
-    res.render("edit.ejs" , {
-        "title" : showtitle
-    });
+app.post("/new", (req, res) => {
+  array.push({
+    id: array.length,
+    title: req.body.title,
+    content: req.body.content,
+  });
+  res.render("index.ejs", {
+    title: array,
+  });
 });
 
+app.post("/edited/:id", (req, res) => {
+  const id_holder2 = array[req.params.id];
+  id_holder2.title = req.body.title;
+  id_holder2.content = req.body.content;
+  res.render("index.ejs" , {
+      title : array
+});
+});
 
+app.get("/edit/:id", (req, res) => {
+  const id_holder = array[req.params.id];
+  res.render("edit.ejs", {
+    id: id_holder.id,
+    title: id_holder.title,
+    content: id_holder.content,
+  });
+});
+app.get("/del", (req, res) => {
+  showcontent = "";
+  showtitle = "";
+  res.render("index.ejs", {
+    title: showtitle,
+    content: showcontent,
+  });
+});
 
-app.listen(port , () => {
-    console.log(`listening on port ${port} !`);
-})
+app.listen(port, () => {
+  console.log(`listening on port ${port} !`);
+});
